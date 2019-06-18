1cd41903b8fd6d519c7e391ff5d47a20b3ad408b
Viktor Speranskyy <vencedor@ukr.net>
2019-06-18 14:26:43 +0300

    OK

---
+++ src/itschool/Main.java
@@ -0,0 +1,15 @@
+package itschool;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner str = new Scanner(System.in);
+        int n = str.nextInt();
+        if (n == 5)
+            System.out.println("=");
+
+    }
+}
